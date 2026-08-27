00fc7068d2b4f283bd8b08a5b14010f455591c54
Janine Vitoria <janinevitoriasp22@gmail>
2026-08-27 17:07:30 -0300

    commit> primeiro

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from 'react';
+import { QuizContext } from './context/quiz';
+
+import Welcome from './components/Welcome'
+import Question from './components/Question';
+
+import './App.css'
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() =>{
+    // embaralhar a pergunta
+  dispatch({type: "REORDER_QUESTIONS"});
+  },[])
+  return (
+   <div className="App">
+    <h1>Quiz de programação</h1>
+    {quizState.gameStage === "Start" && <Welcome/>}
+    {quizState.gameStage === "Playing" && <Question/>}
+
+   </div>
+  )
+}
+
+export default App
